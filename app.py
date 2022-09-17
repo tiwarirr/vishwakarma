5282445e90553786f7286d05e4153eb72a7e873a
RADHA RAMAN TIWARI <70423309+tiwarirr@users.noreply.github.com>
2022-09-17 15:16:31 +0000

    ready to host

--- app.py
+++ app.py
@@ -1,8 +1,46 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,jsonify
 app = Flask(__name__)
+JOBS=[
+  {
+    'id':1,
+    'title':'PGT-PHYSICS',
+    'location':'Mainpuri',
+    'salary':'Rs. 50,000'
+  },
+  {
+    'id':2,
+    'title':'PGT-COMPUTER SCIENCE',
+    'location':'Etawah',
+    'salary':'Rs. 40,000'
+  },
+  {
+    'id':3,
+    'title':'TGT-MUSIC',
+    'location':'Mainpuri',
+    'salary':'Rs. 30,000'
+  },
+  {
+    'id':4,
+    'title':'PGT-HINDI',
+    'location':'Auraiya',
+    'salary':'Rs. 35,000'
+  },
+  {
+    'id':5,
+    'title':'HOSTEL-WARDEN',
+    'location':'Mainpuri',
+    'salary':'Rs. 65,000'
+  }
+]
+
+
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html',jobs=JOBS)
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 print(__name__)
 if (__name__)== "__main__":
